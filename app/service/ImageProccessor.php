3d7954383f06635b9f4955d1979e32aa5f9f936e
a.komarov <spider.kom@gmail.com>
2017-01-20 21:23:35 +0200

    light refactoring, cleaning

--- app/service/ImageProccessor.php
+++ app/service/ImageProccessor.php
@@ -9,43 +9,47 @@ namespace app\service;
  */
 class ImageProccessor {
 
-   var $image;
-   var $type;
-
-   function __construct($filename) {
-      $image_info = getimagesize($filename);
-      $this->type = $image_info[2];
-      if( $this->type == IMAGETYPE_JPEG ) {
-         $this->image = imagecreatefromjpeg($filename);
-      } elseif( $this->type == IMAGETYPE_GIF ) {
-         $this->image = imagecreatefromgif($filename);
-      } elseif( $this->type == IMAGETYPE_PNG ) {
-         $this->image = imagecreatefrompng($filename);
-      }
-   }
-   function save($filename) {
-      if( $this->type == IMAGETYPE_JPEG ) {
-         imagejpeg($this->image,$filename);
-      } elseif( $this->type == IMAGETYPE_GIF ) {
-         imagegif($this->image,$filename);
-      } elseif( $this->type == IMAGETYPE_PNG ) {
-         imagepng($this->image,$filename);
-      }
-   }
+    private $image;
+    private $type;
+    private $width;
+    private $height;
+            
+    public function __construct($filename) {
+        
+        $info = getimagesize($filename);
+        $this->width = $info[0];
+        $this->height = $info[1];
+        $this->type = $info[2];
+        
+        switch ($this->type) {
+            case IMAGETYPE_JPEG: $this->image = imagecreatefromjpeg($filename);
+            break;
+            case IMAGETYPE_GIF: $this->image = imagecreatefromgif($filename);
+            break;
+            case IMAGETYPE_PNG: $this->image = imagecreatefrompng($filename);
+            break;
+            default: break;
+        }
+    }
    
-   function getWidth() {
-      return imagesx($this->image);
-   }
+    public function save($filename) {
+       
+        switch ($this->type) {
+            case IMAGETYPE_JPEG: imagejpeg($this->image,$filename);
+            break;
+            case IMAGETYPE_GIF: imagegif($this->image,$filename);
+            break;
+            case IMAGETYPE_PNG: imagepng($this->image,$filename);
+            break;
+            default: break;
+        }
+    }
    
-   function getHeight() {
-      return imagesy($this->image);
-   }
-   
-   function resize($width,$height) {
-      $new_image = imagecreatetruecolor($width, $height);
-      $ratio =  $this->getWidth() / $width;
-      $scaled_height = min([$this->getheight(), $height* $ratio]);
-      imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $scaled_height);
-      $this->image = $new_image;
-   }
+    public function resize($width, $height) {
+        $new_image = imagecreatetruecolor($width, $height);
+        $ratio =  $this->width / $width;
+        $scaled_height = min([$this->height, $height * $ratio]);
+        imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->width, $scaled_height);
+        $this->image = $new_image;
+    }
 }
