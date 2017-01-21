43e4b2a56e21136537c0513acb41a91ea8e35ea2
a.komarov <spider.kom@gmail.com>
2017-01-21 20:21:51 +0200

    hotfix

--- app/views/layout.php
+++ app/views/layout.php
@@ -28,6 +28,6 @@ use app\controllers\TaskController;
         <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular.min.js"></script>
-        <script src="js/index.js"></script>
+        <script src="/js/index.js"></script>
     </body>
 </html>
