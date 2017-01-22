937341fe5ad9cdce4c01ecd39d0cc2482b89d182
a.komarov <spider.kom@gmail.com>
2017-01-23 01:12:11 +0200

    PHPDoc for App class

--- framework/App.php
+++ framework/App.php
@@ -10,13 +10,16 @@ use framework\components\Router;
  */
 class App {
     
-    //private $router;
+    private static $app;
+    
     private $params;
     private $components;
-    private static $app;
     private $middlewares = [];
     
     
+    /**
+     * @param Array $params Params of apps
+     */
     public function __construct($params = []) {
         session_start();
         $this->addComponent('router', new Router());
@@ -25,22 +28,51 @@ class App {
         self::$app = $this;
     }
     
+    /**
+     * Returns instance of the app
+     * @return App App instance
+     */
     public static function app() {
         return self::$app;
     }
 
+    /**
+     * Shrotcut to the Router's addRoute.
+     * Adds route.
+     * @param String Route pattern
+     * @param Callable $route Array of two elements: class of the controller
+     * and name of action.
+     */
     public function addRoute($route, $action) {
         $this->component('router')->addRoute($route, $action);
     }
 
+    /**
+     * Shrotcut to the Router's addRoutes.
+     * Adds routes.
+     * @param Callable[] $routes Array of routes. Route is array of two elements: 
+     * class of the controller and name of action.
+     */
     public function addRoutes($routes) {
         $this->component('router')->addRoutes($routes);
     }
 
+    /**
+     * Shrotcut to the Router's route.
+     * @param Callable $action Array of two elements: class of the controller
+     * and name of action.
+     * @param Array Params to pass in route.
+     * @return String URL
+     */
     public function route($action, $params = []) {
         return $this->component('router')->route($action, $params);
     }
 
+    /**
+     * Running app. 
+     * Collect middlewares. Pass request and responce through them.
+     * Dispatch url, call correct action for it.
+     */
     public function run() {
         $route_action = $this->component('router')->dispatch($_SERVER['REQUEST_URI']);
         $middlewares = array_merge([
@@ -51,20 +83,41 @@ class App {
         
         $next = array_pop($middlewares);
         echo $next($middlewares);
+        die();
     }
     
+    /**
+     * Returns params
+     * @return Array Array of params
+     */
     public function params() {
         return $this->params;
     }
     
+    /**
+     * DI. Adds any object or mixed value, accessisble 
+     * from any point of the application.
+     * @param String $id Component identifier
+     * @param mixed Component
+     */
     public function addComponent($id, $component) {
         $this->components[$id] = $component;
     }
     
+    /**
+     * DI. Retrieve added component from any point of the application.
+     * @param String $id Component identifier
+     * @return mixed Component
+     */
     public function component($id) {
         return $this->components[$id];
     }
     
+     /**
+     * Redirect
+     * @param Callable $action Array of two elements: class of the controller
+     * and name of action.
+     */
     public function redirect($route) {
         if (is_array($route)) {
             $route = $this->route($route);
