0cce7a2c6a4eb94737c2ff6b86972b73e38d285f
a.komarov <spider.kom@gmail.com>
2017-01-22 19:51:18 +0200

    correct dump

--- tusk.sql
+++ tusk.sql
@@ -1,13 +1,31 @@
 --
--- Table structure for table `tasks`
+-- Структура таблиці `tasks`
 --
 
 CREATE TABLE IF NOT EXISTS `tasks` (
   `id` int(4) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
-  `email` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
-  `text` text CHARACTER SET utf8,
-  `pic` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `text` text,
+  `pic` varchar(50) DEFAULT NULL,
   `done` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
+
+--
+-- Структура таблиці `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `password` int(25) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Дамп даних таблиці `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'admin', 123);
