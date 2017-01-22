7da78ad0e64587630f9d4036d261c3b54d33ed49
a.komarov <spider.kom@gmail.com>
2017-01-23 00:47:04 +0200

    PHPDoc for controllers

--- framework/controllers/Controller.php
+++ framework/controllers/Controller.php
@@ -9,11 +9,23 @@ trait Controller {
     
     use Singleton;
     
+    /**
+     * Render view to layout
+     * @param String $view View name
+     * @param Array $params Params for view
+     * @return String Rendered view inside layout
+     */
     public function render($view, $params = []) {
         $content = $this->renderView($view, $params);
         return $this->renderView('layout.php', ['content' => $content]);
     }
     
+    /**
+     * Render view
+     * @param String $view View name
+     * @param Array $params Params for view
+     * @return String Rendered view
+     */
     public function renderView($view, $params = []) {
         ob_start();
         ob_implicit_flush(false);
@@ -22,6 +34,10 @@ trait Controller {
         return ob_get_clean();
     }
     
+    /**
+     * Redirect to route. Alias for App method
+     * @param mixed $route Callable or url
+     */
     public function redirect($route) {
         App::app()->redirect($route);
     }
